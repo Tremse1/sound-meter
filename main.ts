2ae20b1609e487b47ef664ad60bc80ee5d6ebabe
Tremse1 <90926884+Tremse1@users.noreply.github.com>
2021-10-28 16:08:36 -0400

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 basic.forever(function () {
-    led.plotBarGraph(
-    input.soundLevel(),
-    255
-    )
+    basic.showNumber(input.soundLevel())
+    if (input.soundLevel() >= 125) {
+        music.playMelody("C5 C C5 C C5 C C5 C ", 120)
+    }
 })
